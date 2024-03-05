13b236d4043f798ab531653939799fb5d54d7116
Re1435 <reddymahereddy143@gmail.com>
2024-03-05 18:04:49 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
@@ -9,15 +9,13 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/experience' element={<Experience />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/experience' element={<Experience />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
